ff970aeb3ac58e8ee03d60ada7ad7aa9938701a5
dvirtz <dvirtz@gmail.com>
2016-02-09 21:55:32 +0200

    initial - Foobar2000 SDK + basic plugin

---
+++ googlemusic-foobar/foo_gmusic/main.cpp
@@ -0,0 +1,5 @@
+#include "ATLHelpers.h"
+
+DECLARE_COMPONENT_VERSION("Google Music", "1.0", "enables listening to Google Music(TM) songs on Foobar2000");
+
+VALIDATE_COMPONENT_FILENAME("foo_gmusic.dll");
